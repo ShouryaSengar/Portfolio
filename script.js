67f5cc101095149f3102dcb569fe915fd95b3aac
Shourya Singh Sengar <Shouryacode2021@gmail.com>
2024-03-03 02:35:38 +0530

    Update 2024: V1

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if(window.scrollY > 10){
+
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if (window.scrollY > 480) {
             $('.navbar').addClass("sticky");
         }
-        else{
-            $('.navbar').removeClass("sticky");  
+        else {
+            $('.navbar').removeClass("sticky");
         }
-        if(window.scrollY > 500){
+        if (window.scrollY > 500) {
             $('.scroll-up-btn').addClass("show");
         }
-        else{
+        else {
             $('.scroll-up-btn').removeClass("show");
         }
     });
 
     /* slide-up script */
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
+    $('.scroll-up-btn').click(function () {
+        $('html').animate({ scrollTop: 0 });
     });
 
     /* toggle menu/navbar script */
-    $('.menu-btn').click(function(){
+    $('.menu-btn').click(function () {
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
@@ -40,7 +42,7 @@ $(document).ready(function(){
         backSpeed: 60,
         loop: true
     });
-    
+
     /* OWL Carousel script */
     $('.carousel').owlCarousel({
         margin: 20,
@@ -48,18 +50,34 @@ $(document).ready(function(){
         autoplayTimeOut: 2000,
         autoplayHoverPause: true,
         responsive: {
-            0:{
+            0: {
                 items: 1,
                 nav: false
             },
-            600:{
+            600: {
                 items: 2,
                 nav: false
             },
-            1000:{
+            1000: {
                 items: 3,
                 nav: false
             }
         }
     });
 });
+
+particlesJS("particles-js", { "particles": { "number": { "value": 80, "density": { "enable": true, "value_area": 800 } }, "color": { "value": "#ffffff" }, "shape": { "type": "circle", "stroke": { "width": 0, "color": "#000000" }, "polygon": { "nb_sides": 5 }, "image": { "src": "img/github.svg", "width": 100, "height": 100 } }, "opacity": { "value": 0.8, "random": true, "anim": { "enable": true, "speed": 1, "opacity_min": 0.1, "sync": true } }, "size": { "value": 11.825549493866477, "random": true, "anim": { "enable": true, "speed": 40, "size_min": 0.1, "sync": false } }, "line_linked": { "enable": true, "distance": 150, "color": "#ffffff", "opacity": 0.4, "width": 1 }, "move": { "enable": true, "speed": 6, "direction": "none", "random": false, "straight": false, "out_mode": "out", "bounce": false, "attract": { "enable": false, "rotateX": 600, "rotateY": 1200 } } }, "interactivity": { "detect_on": "canvas", "events": { "onhover": { "enable": true, "mode": "repulse" }, "onclick": { "enable": true, "mode": "push" }, "resize": true }, "modes": { "grab": { "distance": 400, "line_linked": { "opacity": 1 } }, "bubble": { "distance": 400, "size": 40, "duration": 2, "opacity": 8, "speed": 3 }, "repulse": { "distance": 113.43656060348258, "duration": 0.4 }, "push": { "particles_nb": 4 }, "remove": { "particles_nb": 2 } } }, "retina_detect": true });
+var count_particles, stats, update;
+stats = new Stats;
+stats.setMode(0);
+stats.domElement.style.position = 'absolute';
+stats.domElement.style.left = '0px';
+stats.domElement.style.top = '0px';
+document.body.appendChild(stats.domElement);
+count_particles = document.querySelector('.js-count-particles');
+update = function () {
+    stats.begin(); stats.end();
+    if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
+        count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
+    } requestAnimationFrame(update);
+}; requestAnimationFrame(update);;
